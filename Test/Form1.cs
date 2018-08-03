4cbb2f2c065497974810441bc97ccbcabcc0704d
Aceskua <742965106@qq.com>
2018-08-03 09:46:53 +0800

    111

--- Test/Form1.cs
+++ Test/Form1.cs
@@ -121,7 +121,7 @@ namespace Test
             byte[] b = strToToHexByte(ss2);
             byte[] b1 = GetFingerInfo(b);
             string finger = Convert.ToBase64String(b1, 0, b1.Length);
-            string rrr = "";
+            string rrr = "111";
 
             foreach (byte bt in b1)
             {
